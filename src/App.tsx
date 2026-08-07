17d83d3ea6c23aab3f33e12bbd3fbaed5008adce
luiz-davi-m <luizd4398@gmail.com>
2026-08-06 22:20:34 -0300

    utilizando props

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,9 @@ export function App() {
 
   return (
     <>
-      <Heading />
+      <Heading attr={123} attr2="String">
+        Olá mundo!
+      </Heading>
       <p>
         Lorem Ipsum is simply dummy text of the printing and typesetting
         industry. Lorem Ipsum has been the industry's standard dummy text ever
