623d6de225fe9ab44eaddd21063470d22a8f1733
luiz-davi-m <luizd4398@gmail.com>
2026-08-11 20:16:09 -0300

    adicionando estrutura inicial da home

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,29 @@
 import "./styles/theme.css";
 import "./styles/global.css";
-import { Heading } from "./components/Heading";
-import { TimerIcon } from "lucide-react";
 
 export function App() {
-  console.log("App.tsx");
-
   return (
     <>
-      <Heading>
-        Olá mundo!
-        <button>
-          <TimerIcon />
-        </button>
-      </Heading>
-      <p>
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
-        since 1966, when designers at Letraset and James Mosley, the librarian
-        at St Bride Printing Library in London, took a 1914 Cicero translation
-        and scrambled it to make dummy text for Letraset's Body Type sheets. It
-        has survived not only many decades, but also the leap into electronic
-        typesetting, remaining essentially unchanged. It was popularised thanks
-        to these sheets and more recently with desktop publishing software like
-        Aldus PageMaker and Microsoft Word including versions of Lorem Ipsum.
-      </p>
+      <div className="container">
+        <div className="content">
+          <section>LOGO</section>
+        </div>
+      </div>
+      <div className="container">
+        <div className="content">
+          <section>MENU</section>
+        </div>
+      </div>
+      <div className="container">
+        <div className="content">
+          <section>FORM</section>
+        </div>
+      </div>
+      <div className="container">
+        <div className="content">
+          <section>FOOTER</section>
+        </div>
+      </div>
     </>
   );
 }
